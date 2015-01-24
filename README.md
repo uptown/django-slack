0fa4c53eb7788fc08f6f9ed75aef0fb0d53f71ba
Juyoung Lee <uptownlee89@gmail.com>
2015-01-24 15:19:11 +0900

    add read

---
+++ README.md
@@ -0,0 +1,14 @@
+
+django-slack 0.1.0
+=============================
+
+TO DO
+----------------
+
+Targeting Python Version: 2.7, 3.4~
+
+Targeting Django Version: 1.7~
+
+Slack OAuth2 View
+
+Slack Http-based API Client
--- tests.py
+++ tests.py
@@ -7,8 +7,5 @@ class TheTest(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_step1(self):
-        print(SlackClient().oauth2_step1_url(client_id="3319221146.3505511246"))
-
 if __name__ == '__main__':
     unittest.main()
